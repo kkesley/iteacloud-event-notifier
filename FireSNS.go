576479664c12b02a882d8014bb31bb1543d68523
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-02 15:44:47 +1000

    add sns with attribute

--- FireSNS.go
+++ FireSNS.go
@@ -10,8 +10,20 @@ import (
 
 //FireSNS push notification to a topic
 func FireSNS(topic string, message string) error {
+	return FireSNSWithAttribute(topic, message, make(map[string]string))
+}
+
+//FireSNSWithAttribute push notification to a topic with an attribute
+func FireSNSWithAttribute(topic string, message string, attributes map[string]string) error {
 	svc := sns.New(session.New())
 	// params will be sent to the publish call included here is the bare minimum params to send a message.
+	snsAttributes := make(map[string]*sns.MessageAttributeValue)
+	for key, val := range attributes {
+		snsAttributes[key] = &sns.MessageAttributeValue{
+			DataType:    aws.String("String"),
+			StringValue: aws.String(val),
+		}
+	}
 	params := &sns.PublishInput{
 		Message:  aws.String(message), // This is the message itself (can be XML / JSON / Text - anything you want)
 		TopicArn: aws.String(topic),   //Get this from the Topic in the AWS console.
